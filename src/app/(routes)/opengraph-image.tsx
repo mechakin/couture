83d6b97bd5296b992b7bad6e3d52933c940bf45c
mechakin <barrientosryan83@gmail.com>
2023-10-10 10:59:09 -0400

    fix og image

--- src/app/(routes)/opengraph-image.tsx
+++ src/app/(routes)/opengraph-image.tsx
@@ -12,11 +12,6 @@ export const size = {
 
 export const contentType = "image/png";
 
-// Font
-const interBold = fetch(new URL("../../../public/Inter-Bold.ttf", import.meta.url)).then(
-  (res) => res.arrayBuffer()
-);
-
 // Image generation
 export default async function Image() {
   return new ImageResponse(
@@ -42,14 +37,6 @@ export default async function Image() {
       // For convenience, we can re-use the exported opengraph-image
       // size config to also set the ImageResponse's width and height.
       ...size,
-      fonts: [
-        {
-          name: "Inter",
-          data: await interBold,
-          style: "normal",
-          weight: 600,
-        },
-      ],
     }
   );
 }
